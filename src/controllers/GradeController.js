7598eaee82d573414475fa1fed568746bb7d0ea3
lucasbezerra <lucasbezerra@serel.com.br>
2020-06-08 20:07:35 -0300

    Rota Delete e Update finalizadas

--- src/controllers/GradeController.js
+++ src/controllers/GradeController.js
@@ -33,4 +33,70 @@ module.exports = {
       console.log(error);
     }
   },
+
+  update(req, res) {
+    const { id } = req.params;
+    const { student, subject, type, value } = req.body;
+
+    const data = JSON.parse(
+      fs.readFileSync(path.resolve(__dirname, '..', 'data', 'grades.json'))
+    );
+
+    const grade = data.grades.find((grade) => grade.id === +id);
+
+    if (!grade) {
+      return res.status(404).json({ error: 'Grade não encontrada' });
+    }
+
+    if (student) {
+      grade.student = student;
+    }
+
+    if (subject) {
+      grade.subject = subject;
+    }
+    if (type) {
+      grade.type = type;
+    }
+    if (value) {
+      grade.value = value;
+    }
+
+    fs.writeFile(
+      path.resolve(__dirname, '..', 'data', 'grades.json'),
+      JSON.stringify(data),
+      'utf8',
+      (err) => {
+        if (err) throw err;
+      }
+    );
+
+    return res.status(200).json(grade);
+  },
+
+  delete(req, res) {
+    const { id } = req.params;
+
+    const data = JSON.parse(
+      fs.readFileSync(path.resolve(__dirname, '..', 'data', 'grades.json'))
+    );
+
+    const newGrades = data.grades.filter((grade) => grade.id !== +id);
+
+    const newData = {
+      nextId: data.nextId + 1,
+      grades: newGrades,
+    };
+
+    fs.writeFile(
+      path.resolve(__dirname, '..', 'data', 'grades.json'),
+      JSON.stringify(newData),
+      'utf8',
+      (err) => {
+        if (err) throw err;
+      }
+    );
+
+    return res.json({ msg: 'Grade excluida com sucesso' });
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,7 @@ const GradeController = require('./controllers/GradeController');
 const routes = express.Router();
 
 routes.post('/v1/grade', GradeController.create);
+routes.put('/v1/grade/:id', GradeController.update);
+routes.delete('/v1/grade/:id', GradeController.delete);
 
 module.exports = routes;
